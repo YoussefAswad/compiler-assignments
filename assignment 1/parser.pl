fbdd9ddbf466d38676ceaa53b9fbdbee37e1579e
YoussefAswad <85849220+YoussefAswad@users.noreply.github.com>
2023-05-03 03:11:36 +0300

    Parse tree working

--- assignment 1/parser.pl
+++ assignment 1/parser.pl
@@ -49,64 +49,69 @@
 % number(number(A)) --> [X], {integer(X), X >= 0, A = X}.
 
 
-s --> statement.
-s --> statement,s.
+s(S) --> statement(S).
+s((S,R)) --> statement(S),s(R).
 
-statement --> assignment.
-statement --> conditional.
-statement --> loop.
+statement(assignment(A)) --> assignment(A).
+statement(conditional(C)) --> conditional(C).
+statement(loop(L)) --> loop(L).
 
-assignment --> identifier,[=], arithmetic_expression,[;].
-conditional --> if_statement.
-conditional --> if_else_statement.
-loop --> [while], condition, statement.
+assignment((Id,[=],E)) --> identifier(Id),[=], arithmetic_expression(E),[;].
+conditional(if(IF)) --> if_statement(IF).
+conditional(if_else(C)) --> if_else_statement(C).
+loop(while([while],C,S)) --> [while], condition(C), statement(S).
 
-if_statement --> [if], condition, statement.
-if_else_statement --> if_statement, [else],statement.
+if_statement((C,S)) --> [if], condition(C), statement(S).
+if_else_statement((IF,[else],S)) --> if_statement(IF), [else],statement(S).
 
-condition --> ['('], relative_expression, [')'].
+condition(C) --> ['('], relative_expression(C), [')'].
 
-relative_expression --> arithmetic_expression, rel_operator, arithmetic_expression.
+relative_expression(rel(E1,O,E2)) --> arithmetic_expression(E1), rel_operator(O), arithmetic_expression(E2).
 
 
 % arithmetic expression
-arithmetic_expression --> term.
-arithmetic_expression -->  ['('], arithmetic_expression, [')'].
-arithmetic_expression --> term, arith_operator, arithmetic_expression.
+arithmetic_expression(arith(T)) --> term(T).
+arithmetic_expression(arith(E)) -->  ['('], arithmetic_expression(E), [')'].
+arithmetic_expression(arith(T,O,E)) --> term(T), arith_operator(O), arithmetic_expression(E).
 
 % term
-term --> identifier.
-term --> number.
+term(term(Id)) --> identifier(Id).
+term(term(N)) --> number(N).
 
 % lexicon
-identifier --> first_character, characters.
+identifier(id(FCh,Ch)) --> first_character(FCh), characters(Ch).
 
-first_character --> letter.
-first_character --> symbol.
+first_character(L) --> letter(L).
+first_character(Sy) --> symbol(Sy).
 
-characters --> [].
-characters --> character, characters.
+% characters([]) --> [].
+% characters((FCh,Ch)) --> character(FCh), characters(Ch).
 
-character --> letter.
-character --> symbol.
-character --> number.
+characters([]) --> [].
+characters([H|T]) --> [H], {atom_length(H,Len),Len<2,char_type(H, alnum); member(H,[$,'_'])}, characters(T).
 
-letter --> [C], {atom_chars(C, [Char]), char_type(Char, alpha)}.
-letter --> [C], {atom_length(C,Len),Len<2, char_type(C, alpha)}.
+
+character(C) --> letter(C).
+character(C) --> symbol(C).
+character(C) --> number(C).
+
+% letter(C) --> [C], {atom_chars(C, [Char]), char_type(Char, alpha)}.
+letter(C) --> [C], {atom_length(C,Len),Len<2, char_type(C, alpha)}.
 % letter --> [C], {member(C, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])}.
-symbol --> [C], {member(C, [$,'_'])}.
-number --> [X], {integer(X), X >= 0}.
+symbol(C) --> [C], {member(C, [$,'_'])}.
+number(X) --> [X], {integer(X), X >= 0}.
 
-arith_operator --> [*].
-arith_operator --> [/].
-arith_operator --> ['%'].
+arith_operator(O) --> [O], {member(O, ['+','-','*','/','%'])}.
+% arith_operator --> [*].
+% arith_operator --> [/].
+% arith_operator --> ['%'].
+% arith_operator --> [+].
+% arith_operator --> [-].
 
-arith_operator --> [+].
-arith_operator --> [-].
-
-rel_operator --> [>].
-rel_operator --> [<].
-rel_operator --> [==].
-rel_operator --> ['!='].
-rel_operator --> [>=].
-rel_operator --> [<=].
+rel_operator(O) --> [O], {member(O, ['>','<','==','!=','>=','<='])}.
+% rel_operator --> [>].
+% rel_operator --> [<].
+% rel_operator --> [==].
+% rel_operator --> ['!='].
+% rel_operator --> [>=].
+% rel_operator --> [<=].
